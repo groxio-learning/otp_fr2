4cfbfd4b162763595f3787775558c7090cf02d0b
Jim Alexander <jalexander@adobe.com>
2022-06-06 11:47:43 -0700

    add client side API

--- counter/lib/counter/service.ex
+++ counter/lib/counter/service.ex
@@ -1,6 +1,10 @@
 defmodule Counter.Service do
   alias Counter.Core
 
+  def loop(count) do
+    count |> listen |> loop
+  end
+
   def listen(count) do
     receive do
       :inc ->
@@ -9,6 +13,25 @@ defmodule Counter.Service do
       {:show, from_pid} ->
         send(from_pid, Core.show(count))
         count
+    end
+  end
+
+  # client side API
+  def start(input) do
+    spawn fn ->
+      input |> Core.new |> loop
+    end
+  end
+
+  def inc(counter_pid) do
+    send(counter_pid, :inc)
+    :ok
+  end
+
+  def show(counter_pid) do
+    send(counter_pid, {:show, self()})
+    receive do
+      count -> count
     end
   end
 end
