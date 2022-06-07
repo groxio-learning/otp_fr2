43465bc5d2473b2f4ef92eb6c32e5e90e87856d4
Kate Brender <kate.brender@gmail.com>
2022-06-07 14:03:03 -0400

    Add score new() and show()

---
+++ enigma/lib/enigma/game/score.ex
@@ -0,0 +1,17 @@
+defmodule Enigma.Game.Score do
+
+	defstruct [:red, :white]
+
+	def new(answer, attempt) do
+		reds = Enum.zip(answer, attempt) |> Enum.filter(fn {a, g} -> a == g end) |> Enum.count
+		misses = attempt -- answer
+		whites = Enum.count(answer) - reds - Enum.count(misses)
+		%__MODULE__{red: reds, white: whites}
+	end
+
+	def show(%__MODULE__{red: reds, white: whites}) do
+		# reds.times do | "R"
+		0..(reds-1) |> Enum.map(fn _x -> "R" end) |> join()
+		0..(whites-1) |> Enum.map(fn _x -> "W" end) |> join()
+	end
+end
