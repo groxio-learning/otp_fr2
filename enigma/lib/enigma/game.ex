4bf548e1d6cd94a905f8e3938dbb231c69bb9b82
Jesse Kelly <jekelly@adobe.com>
2022-06-07 13:44:18 -0500

    Add render stub and won? implementation

--- enigma/lib/enigma/game.ex
+++ enigma/lib/enigma/game.ex
@@ -12,4 +12,15 @@ defmodule Enigma.Game do
   def guess(%__MODULE__{} = game, attempt) do
     %{game | attempts: [attempt | game.attempts]}
   end
+
+  def render(game) do
+    """
+    1 2 3 4 | RW
+    4 1 5 3 | RRWW
+    Playing
+    """
+  end
+
+  defp won?(%__MODULE__{answer: answer, attempts: [answer | _rest]}), do: true
+  defp won?(_game), do: false
 end
